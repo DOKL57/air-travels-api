7663e0b04eee1b1cadd943a875390b89b8f41c06
daniel <daniil2705@gmail.com>
2022-07-04 06:58:13 -0400

    Initial commit with docker-compose and migration

---
+++ src/main/java/com/dokl57/airtravelsapi/AirTravelsApiApplication.java
@@ -0,0 +1,13 @@
+package com.dokl57.airtravelsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTravelsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirTravelsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dokl57/airtravelsapi/AirTravelsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokl57.airtravelsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTravelsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
