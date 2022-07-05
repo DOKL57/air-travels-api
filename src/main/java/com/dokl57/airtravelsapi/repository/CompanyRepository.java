8cb228987f9899b6f104408b87de9dcc6f78c972
daniel <daniil2705@gmail.com>
2022-07-05 04:53:45 -0400

    change return type in company repository

--- src/main/java/com/dokl57/airtravelsapi/repository/CompanyRepository.java
+++ src/main/java/com/dokl57/airtravelsapi/repository/CompanyRepository.java
@@ -2,7 +2,11 @@ package com.dokl57.airtravelsapi.repository;
 
 import com.dokl57.airtravelsapi.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
+    Optional<Company> findCompanyByName(String name);
 }
